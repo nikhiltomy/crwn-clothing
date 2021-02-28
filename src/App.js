86f6a0cae5874d026ce04a12d6f5cad26e5183fd
nikhil <nikhiltomy@au-ki.com>
2021-03-01 01:52:05 +0530

    shop page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import './App.css';
-import {HomePage} from './pages/homepage/homepage.component'
+import {HomePage} from './pages/homepage/homepage.component';
+import {ShopPage} from './pages/shop-page/shop-page.component.jsx';
 import {Route,BrowserRouter, Switch} from 'react-router-dom';
-
- const HatsPage=(props)=>{
-   console.log(props)
-  return(
-    <div>
-      <h1>Hat page</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Switch>
-      <Route exact path="/">{HomePage}</Route>
-      <Route path="/hats">{HatsPage}</Route>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/shop" component={ShopPage}/>
       </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { CollectionItem } from "../collection-item/collection-item.component";
+import './collection-preview.styles.scss'
+export  function CollectionPreview({ title, items,}) {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title}</h1>
+      <div className="preview">
+        {items.filter((item,index)=>index<4).map(({id,...othercollectionitem}) => (
+          <CollectionItem key={id} {...othercollectionitem} />
+        ))}
+      </div>
+    </div>
+  );
+}
